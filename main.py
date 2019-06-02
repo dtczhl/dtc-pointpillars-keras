2d35145599bde04b5339d7323d4eb4057f8d5c19
Huanle Zhang <zhanghuanle1342@gmail.com>
2019-06-02 10:50:42 -0700

    coexistence

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from model import make_pillar_feature_net
+from model import make_pillar_feature_net, make_backbone
 import numpy as np
 
 P = 2
@@ -6,16 +6,24 @@ C = 64
 N = 100
 D = 9
 
-x = np.random.random((P, N, D))
-y = np.random.random((P, 1, C))
+# x = np.random.random((P, N, D))
+# y = np.random.random((P, 1, C))
 
 pillar_feature_net = make_pillar_feature_net()
 
 pillar_feature_net.compile('sgd', 'mse')
-pillar_feature_net.fit(x, y)
+pillar_feature_net.summary()
+pillar_feature_net.save('Model/pointpillars_pfn' + '_C' + str(C) + '_N' + str(N) + '.h5')
+print('Pillar PFN Done!')
 
-pillar_feature_net.save('Model/pointpillars' + '_C' + str(C) + '_N' + str(N) + '.h5')
-print('Done!')
+
+backbone_model = make_backbone(80, 64, 64)
+backbone_model.compile('sgd', 'mse')
+backbone_model.summary()
+backbone_model.save('Model/pointpillars_backbone' + '_W' + str(80) + '_H' + str(64) + '_C' + str(64) + '.h5')
+print('Pillar Backbone Done!')
+
+
 
 
 
--- model.py
+++ model.py
@@ -1,5 +1,12 @@
 from keras.models import Model
-from keras.layers import Input, Conv1D, BatchNormalization, MaxPool1D, Conv2D, Deconv2D, Concatenate, Dense
+from keras.layers import Input, Conv1D, BatchNormalization, MaxPool1D, Conv2D, Deconv2D, Concatenate, Lambda, MaxPooling2D, ZeroPadding2D, Reshape, Activation, Conv2DTranspose
+from keras import backend as K
+
+
+from keras_layers.keras_layer_AnchorBoxes import AnchorBoxes
+from keras_layers.keras_layer_L2Normalization import L2Normalization
+from keras_layers.keras_layer_DecodeDetections import DecodeDetections
+from keras_layers.keras_layer_DecodeDetectionsFast import DecodeDetectionsFast
 
 D = 9
 N = 100
@@ -15,26 +22,149 @@ def make_pillar_feature_net():
 
 
 def make_backbone(width, height, feature_C):
-    x = Input(shape=(width, height, feature_C), name='input')
-    conv_1 = Conv2D(C, 3, strides=2, padding='same', activation='relu', name='conv_1')(x)
-    bn_conv_1 = BatchNormalization(name='bn_conv_1')(conv_1)
-    conv_2 = Conv2D(2*C, 3, strides=2, padding='same', activation='relu', name='conv_2')(bn_conv_1)
-    bn_conv_2 = BatchNormalization(name='bn_conv_2')(conv_2)
-    conv_3 = Conv2D(4*C, 3, strides=2, padding='same', activation='relu', name='conv_3')(bn_conv_2)
-    bn_conv_3 = BatchNormalization(name='bn_conv_3')(conv_3)
+    x = Input(shape=(height, width, feature_C), name='input')
 
-    deconv_1 = Deconv2D(2 * C, 3, strides=1, padding='same', activation='relu', name='deconv_1')(bn_conv_1)
-    bn_deconv_1 = BatchNormalization(name='bn_deconv_1')(deconv_1)
+    y = rpn_conv_block(x, feature_C, 3, name='rpn_conv1')
+    y_deconv1 = Conv2DTranspose(2*feature_C, 3, strides=1, padding='same', name='rpn_deconv1')(y)
+    y = rpn_conv_block(y, 2*feature_C, 5, name='rpn_conv2')
+    y_deconv2 = Conv2DTranspose(2*feature_C, 2, strides=2, padding='same', name='rpn_deconv2')(y)
+    y = rpn_conv_block(y, 4*feature_C, 5, name='rpn_conv3')
+    y = Conv2DTranspose(2*feature_C, 4, strides=4, padding='same', name='rpn_deconv3')(y)
+    y = Concatenate()([y, y_deconv2, y_deconv1])
 
-    deconv_2 = Deconv2D(2 * C, 3, strides=2, padding='same', activation='relu', name='deconv_2')(bn_conv_2)
-    bn_deconv_2 = BatchNormalization(name='bn_deconv_2')(deconv_2)
+    print (K.int_shape(y))
 
-    deconv_3 = Deconv2D(2 * C, 3, strides=4, padding='same', activation='relu', name='deconv_3')(bn_conv_3)
-    bn_deconv_3 = BatchNormalization(name='bn_deconv_3')(deconv_3)
+    return Model(inputs=x, outputs=ssd_300(y))
 
-    concat_1 = Concatenate(name='concat_1')([bn_deconv_1, bn_deconv_2, bn_deconv_3])
 
-    return Model(inputs=x, outputs=concat_1)
+def _rpn_conv(x, cout, k, s, p, name, i=None):
+    if i is not None:
+        name += '_blk' + str(i)
+    x = ZeroPadding2D(p, name=name + '_pad')(x)
+    x = Conv2D(cout, k, strides=s, name=name + '_conv')(x)
+    x = BatchNormalization(name=name + '_bn')(x)
+    x = Activation('relu', name=name + '_relu')(x)
+    return x
+
+
+def rpn_conv_block(x, cout, q, name='rpn_conv'):
+    x = _rpn_conv(x, cout, 3, 2, 1, name)
+    for i in range(q):
+        x = _rpn_conv(x, cout, 3, 1, 1, name, i + 1)
+    return x
+
+
+def ssd_300(x):
+    n_classes = 2
+    two_boxes_for_ar1 = True
+    aspect_ratios_per_layer = [[1.0, 2.0, 0.5],
+                               [1.0, 2.0, 0.5, 3.0, 1.0/3.0],
+                                [1.0, 2.0, 0.5, 3.0, 1.0/3.0],
+                                [1.0, 2.0, 0.5, 3.0, 1.0/3.0],
+                                [1.0, 2.0, 0.5],
+                                [1.0, 2.0, 0.5]]
+
+
+    n_boxes = []
+    for ar in aspect_ratios_per_layer:
+        if (1 in ar) & two_boxes_for_ar1:
+            n_boxes.append(len(ar) + 1)  # +1 for the second box for aspect ratio 1
+        else:
+            n_boxes.append(len(ar))
+
+    conv1_1 = Conv2D(64, (3, 3), activation='relu', padding='same', name='conv1_1')(x)
+    conv1_2 = Conv2D(64, (3, 3), activation='relu', padding='same', name='conv1_2')(conv1_1)
+    pool1 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same', name='pool1')(conv1_2)
+
+    conv2_1 = Conv2D(128, (3, 3), activation='relu', padding='same', name='conv2_1')(pool1)
+    conv2_2 = Conv2D(128, (3, 3), activation='relu', padding='same', name='conv2_2')(conv2_1)
+    pool2 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same', name='pool2')(conv2_2)
+
+    conv3_1 = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_1')(pool2)
+    conv3_2 = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_2')(conv3_1)
+    conv3_3 = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv3_3')(conv3_2)
+    pool3 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same', name='pool3')(conv3_3)
+
+    conv4_1 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_1')(pool3)
+    conv4_2 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_2')(conv4_1)
+    conv4_3 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv4_3')(conv4_2)
+    pool4 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same', name='pool4')(conv4_3)
+
+    conv5_1 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_1')(pool4)
+    conv5_2 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_2')(conv5_1)
+    conv5_3 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv5_3')(conv5_2)
+    pool5 = MaxPooling2D(pool_size=(3, 3), strides=(1, 1), padding='same', name='pool5')(conv5_3)
+
+    fc6 = Conv2D(1024, (3, 3), dilation_rate=(6, 6), activation='relu', padding='same', name='fc6')(pool5)
+
+    fc7 = Conv2D(1024, (1, 1), activation='relu', padding='same', name='fc7')(fc6)
+
+    conv6_1 = Conv2D(256, (1, 1), activation='relu', padding='same', name='conv6_1')(fc7)
+    conv6_1 = ZeroPadding2D(padding=((1, 1), (1, 1)), name='conv6_padding')(conv6_1)
+    conv6_2 = Conv2D(512, (3, 3), strides=(2, 2), activation='relu', padding='valid', name='conv6_2')(conv6_1)
+
+    conv7_1 = Conv2D(128, (1, 1), activation='relu', padding='same', name='conv7_1')(conv6_2)
+    conv7_1 = ZeroPadding2D(padding=((1, 1), (1, 1)), name='conv7_padding')(conv7_1)
+    conv7_2 = Conv2D(256, (3, 3), strides=(2, 2), activation='relu', padding='valid', name='conv7_2')(conv7_1)
+
+    conv8_1 = Conv2D(128, (1, 1), activation='relu', padding='same', name='conv8_1')(conv7_2)
+    conv8_2 = Conv2D(256, (3, 3), strides=(1, 1), activation='relu', padding='valid', name='conv8_2')(conv8_1)
+
+    conv9_1 = Conv2D(128, (1, 1), activation='relu', padding='same', name='conv9_1')(conv8_2)
+    conv9_2 = Conv2D(256, (3, 3), strides=(1, 1), activation='relu', padding='valid', name='conv9_2')(conv9_1)
+
+    # Feed conv4_3 into the L2 normalization layer
+    conv4_3_norm = L2Normalization(gamma_init=20, name='conv4_3_norm')(conv4_3)
+
+    conv4_3_norm_mbox_conf = Conv2D(n_boxes[0] * n_classes, (3, 3), padding='same', name='conv4_3_norm_mbox_conf')(conv4_3_norm)
+    fc7_mbox_conf = Conv2D(n_boxes[1] * n_classes, (3, 3), padding='same', name='fc7_mbox_conf')(fc7)
+    conv6_2_mbox_conf = Conv2D(n_boxes[2] * n_classes, (3, 3), padding='same', name='conv6_2_mbox_conf')(conv6_2)
+    conv7_2_mbox_conf = Conv2D(n_boxes[3] * n_classes, (3, 3), padding='same', name='conv7_2_mbox_conf')(conv7_2)
+    conv8_2_mbox_conf = Conv2D(n_boxes[4] * n_classes, (3, 3), padding='same', name='conv8_2_mbox_conf')(conv8_2)
+    conv9_2_mbox_conf = Conv2D(n_boxes[5] * n_classes, (3, 3), padding='same', name='conv9_2_mbox_conf')(conv9_2)
+    # We predict 4 box coordinates for each box, hence the localization predictors have depth `n_boxes * 4`
+    # Output shape of the localization layers: `(batch, height, width, n_boxes * 4)`
+    conv4_3_norm_mbox_loc = Conv2D(n_boxes[0] * 4, (3, 3), padding='same', name='conv4_3_norm_mbox_loc')(conv4_3_norm)
+    fc7_mbox_loc = Conv2D(n_boxes[1] * 4, (3, 3), padding='same', name='fc7_mbox_loc')(fc7)
+    conv6_2_mbox_loc = Conv2D(n_boxes[2] * 4, (3, 3), padding='same', name='conv6_2_mbox_loc')(conv6_2)
+    conv7_2_mbox_loc = Conv2D(n_boxes[3] * 4, (3, 3), padding='same', name='conv7_2_mbox_loc')(conv7_2)
+    conv8_2_mbox_loc = Conv2D(n_boxes[4] * 4, (3, 3), padding='same', name='conv8_2_mbox_loc')(conv8_2)
+    conv9_2_mbox_loc = Conv2D(n_boxes[5] * 4, (3, 3), padding='same', name='conv9_2_mbox_loc')(conv9_2)
+
+    conv4_3_norm_mbox_conf_reshape = Reshape((-1, n_classes), name='conv4_3_norm_mbox_conf_reshape')(
+        conv4_3_norm_mbox_conf)
+    fc7_mbox_conf_reshape = Reshape((-1, n_classes), name='fc7_mbox_conf_reshape')(fc7_mbox_conf)
+    conv6_2_mbox_conf_reshape = Reshape((-1, n_classes), name='conv6_2_mbox_conf_reshape')(conv6_2_mbox_conf)
+    conv7_2_mbox_conf_reshape = Reshape((-1, n_classes), name='conv7_2_mbox_conf_reshape')(conv7_2_mbox_conf)
+    conv8_2_mbox_conf_reshape = Reshape((-1, n_classes), name='conv8_2_mbox_conf_reshape')(conv8_2_mbox_conf)
+    conv9_2_mbox_conf_reshape = Reshape((-1, n_classes), name='conv9_2_mbox_conf_reshape')(conv9_2_mbox_conf)
+    # Reshape the box predictions, yielding 3D tensors of shape `(batch, height * width * n_boxes, 4)`
+    # We want the four box coordinates isolated in the last axis to compute the smooth L1 loss
+    conv4_3_norm_mbox_loc_reshape = Reshape((-1, 4), name='conv4_3_norm_mbox_loc_reshape')(conv4_3_norm_mbox_loc)
+    fc7_mbox_loc_reshape = Reshape((-1, 4), name='fc7_mbox_loc_reshape')(fc7_mbox_loc)
+    conv6_2_mbox_loc_reshape = Reshape((-1, 4), name='conv6_2_mbox_loc_reshape')(conv6_2_mbox_loc)
+    conv7_2_mbox_loc_reshape = Reshape((-1, 4), name='conv7_2_mbox_loc_reshape')(conv7_2_mbox_loc)
+    conv8_2_mbox_loc_reshape = Reshape((-1, 4), name='conv8_2_mbox_loc_reshape')(conv8_2_mbox_loc)
+    conv9_2_mbox_loc_reshape = Reshape((-1, 4), name='conv9_2_mbox_loc_reshape')(conv9_2_mbox_loc)
+    # Reshape the anchor box tensors, yielding 3D tensors of shape `(batch, height * width * n_boxes, 8)`
+
+    mbox_conf = Concatenate(axis=1, name='mbox_conf')([conv4_3_norm_mbox_conf_reshape,
+                                                       fc7_mbox_conf_reshape,
+                                                       conv6_2_mbox_conf_reshape,
+                                                       conv7_2_mbox_conf_reshape,
+                                                       conv8_2_mbox_conf_reshape,
+                                                       conv9_2_mbox_conf_reshape])
+
+    mbox_loc = Concatenate(axis=1, name='mbox_loc')([conv4_3_norm_mbox_loc_reshape,
+                                                     fc7_mbox_loc_reshape,
+                                                     conv6_2_mbox_loc_reshape,
+                                                     conv7_2_mbox_loc_reshape,
+                                                     conv8_2_mbox_loc_reshape,
+                                                     conv9_2_mbox_loc_reshape])
+
+    mbox_conf_softmax = Activation('softmax', name='mbox_conf_softmax')(mbox_conf)
+    predictions = Concatenate(axis=2, name='predictions')([mbox_conf_softmax, mbox_loc])
+    return predictions
 
 
 if __name__ == '__main__':
@@ -42,7 +172,7 @@ if __name__ == '__main__':
     # pillar_feature_net_model = make_pillar_feature_net()
     # pillar_feature_net_model.summary()
 
-    backbone_model = make_backbone(120, 80, 64)
+    backbone_model = make_backbone(440, 800, 64)
     backbone_model.summary()
 
 
